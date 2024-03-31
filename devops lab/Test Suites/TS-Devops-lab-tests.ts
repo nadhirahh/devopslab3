3b20d6cdc638415295c5778af8c6b142d5bc7a5a
hidayatul nadhirah <nadhirahh97@gmail.com>
2024-04-01 07:36:27 +0800

    update

---
+++ devops lab/Test Suites/TS-Devops-lab-tests.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-Devops-lab-tests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>69d86fb7-1344-46a8-b32d-3a5bb5480db0</testSuiteGuid>
+   <testCaseLink>
+      <guid>5d1e4952-1a86-458c-8b5b-c579a91f3340</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/First test case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
